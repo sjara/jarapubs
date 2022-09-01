f12327ff234c977a570adeb39f5901bc419eee8a
Jenny Mohn <jlmohn@ucdavis.edu>
2022-09-01 16:22:15 -0700

    updated to generate just for new subjects

--- 2022paspeech/database_speech_selectivity.py
+++ 2022paspeech/database_speech_selectivity.py
@@ -25,8 +25,8 @@ reload(studyutils)
 
 
 databaseDir = os.path.join(settings.DATABASE_PATH, studyparams.STUDY_NAME)
-allSubjects = studyparams.EPHYS_MICE
-#allSubjects = studyparams.TEST_MOUSE
+#allSubjects = studyparams.EPHYS_MICE
+allSubjects = ['feat009', 'feat010']
 
 
 
