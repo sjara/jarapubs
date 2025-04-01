d6f8ef8cc57a132f1548b97ef1dc612791a8dc7d
sjara <sjara@uoregon.edu>
2025-03-31 17:09:46 -0700

    Added folder for 2024traje.

---
+++ 2024traje/studyparams.py
@@ -0,0 +1,14 @@
+"""
+This file contains the default names and queries used for this study.
+"""
+
+STUDY_NAME = '2024traje'
+
+# List of animals used in this study
+SUBJECTS = ['feat014', 'feat015', 'feat016', 'feat017', 'feat018', 'feat019']
+
+# -- About natural sounds --
+SOUND_CATEGORIES = ['Frogs', 'Crickets', 'Streamside', 'Bubbling', 'Bees']
+#INSTANCES_PER_CATEGORY = 4
+
+# Only two categories: 01-'Crickets', 03-'Bubbling'
